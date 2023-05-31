e024b1e63800337c5326cec6e31360915a54a679
j.TRUbela <81325704+jtrubela@users.noreply.github.com>
2023-05-31 07:06:02 -0400

    Add files via upload

---
+++ DartsCheckoutsGame/CheckOUTS/CheckOUTS/DartsCheckoutsGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  DartsCheckoutsGameApp.swift
+//  DartsCheckoutsGame
+//
+//  Created by Justin Trubela on 5/14/23.
+//
+
+import SwiftUI
+
+@main
+struct CheckOUTS: App {
+    var body: some Scene {
+        WindowGroup {
+            LandingPageView()
+        }
+    }
+}
---
+++ DartsCheckoutsGame/README.md
@@ -0,0 +1,2 @@
+# DartsCheckoutsGame
+This is the project I created for my Human To Computer Interaction to emulate a darts score keeping game.
