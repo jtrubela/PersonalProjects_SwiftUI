9c6ecbb9c7b12ed8dbb2a0a8dd5415dd8578bec7
Justin Trubela <jtrubela@icloud.com>
2023-08-06 19:40:40 -0400

    Updated a repository that i cloned and gave access via a remote ssh key

---
+++ Stash House - Item Tracker Inventory/Stash House/Views/CardCollectionView.swift
@@ -0,0 +1,8 @@
+//
+//  CardCollectionView.swift
+//  Stash House
+//
+//  Created by Justin Trubela on 8/6/23.
+//
+
+import Foundation
--- Stash House - Item Tracker Inventory/Stash House/Views/Collection Lists/View Models/ListItem/Models/Cards/View/CardListItem.swift
+++ Stash House - Item Tracker Inventory/Stash House/Views/Collection Lists/View Models/ListItem/Models/Cards/View/CardListItem.swift
@@ -22,6 +22,12 @@ struct CardListItemImage: View {
         }
     }
 }
+struct CardListItemImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CardListItemImage(image: Image(systemName:"trash.fill"))
+        
+    }
+}
 
 struct CardListItemInfo: View {
     let card: Topps88CGCard
@@ -54,12 +60,30 @@ struct CardListItemInfo: View {
     }
 }
 
+struct CardListItemInfo_Previews: PreviewProvider {
+    static var previews: some View {
+        HStack{
+            CardListItemInfo(card: Topps88CGCard(cardNumber: 1, cardTitle: "testing 1"), cardTitle: "MY TEST", cardNumber: 999)
+        }
+    }
+}
+
 struct CardListItemView: View {
     let card: Topps88CGCard
     
     var body: some View {
         HStack{
             CardListItemInfo(card: card, cardTitle: card.cardTitle, cardNumber: card.cardNumber)
+        }
+    }
+}
+struct CardListItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        HStack{
+            CardListItemImage(image: Image(systemName:"trash.fill"))
+                .padding(10)
+//            CardListItemInfo(card: Topps88CGCard(cardNumber: 1, cardTitle: "testing 1"), cardTitle: "MY TEST", cardNumber: 999)
+            CardListItemView(card: Topps88CGCard(cardNumber: 1, cardTitle: "testing 1"))
         }
     }
 }
@@ -87,5 +111,21 @@ struct CardListInfoView: View {
             }
         }
         .padding()
+    }
+}
+struct CardListInfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        VStack{
+            HStack{
+                CardListItemImage(image: Image(systemName:"trash.fill"))
+                    .padding(10)
+                CardListItemInfo(card: Topps88CGCard(cardNumber: 1, cardTitle: "testing 1"), cardTitle: "MY TEST", cardNumber: 999)
+            }
+            ZStack{
+                
+                CardListItemView(card:  Topps88CGCard(cardNumber: 1, cardTitle: "testing 1"))
+                CardListItemImage(image: Image(systemName:"trash.fill"))
+            }
+        }
     }
 }
